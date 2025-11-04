8b5e83d48cafa9b59743841d8a88cf390cc5c404
The House Elf <andrewm@chinsegut.com>
2025-11-03 16:52:50 -0800

    Add:  missing docstrings

--- kanji_time/__main__.py
+++ kanji_time/__main__.py
@@ -1,6 +1,20 @@
+"""
+Top level entry point to Kanji Time via "Python -m"
+
+.. seealso:: `__main__.py <https://docs.python.org/3.11/library/__main__.html#main-py-in-python-packages>`_
+
+This entry point transfers control to the Kanji Time CLI in :mod:`kanji_time_cli`.
+"""
+
 def main():
+    """
+    Define the module entry point.
+
+    Immediately transfers control to the Kanji Time CLI.
+    """
     from kanji_time.kanji_time_cli import cli_entry_point
     cli_entry_point()
+
 
 if __name__ == "__main__":
     main()
